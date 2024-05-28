cf4e538ae9e6d6643bc17392c42aa866ec24d422
Chris Muzzin <muzzz@Chriss-MBP.attlocal.net>
2024-05-28 15:48:43 -0700

    pushing changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { routes } from './app.routes';
 import { IconsModule } from './icons/icons.module';
 
@@ -8,6 +8,15 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    importProvidersFrom([IconsModule, FontAwesomeModule]),
+    provideFirebaseApp(() => initializeApp({ 
+      apiKey: "AIzaSyAzssf1YYLC68y7HlKtl-0DM9X-fkF_f_M",
+      authDomain: "website-444c9.firebaseapp.com",
+      projectId: "website-444c9",
+      storageBucket: "website-444c9.appspot.com",
+      messagingSenderId: "526849955630",
+      appId: "1:526849955630:web:c1b9aa31c4f53c8d8f8cbf",
+      measurementId: "G-90F3KNEG33"
+     })),
+    importProvidersFrom([IconsModule]),
   ]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconsModule } from '../icons/icons.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IconsModule, FontAwesomeModule],
+  imports: [IconsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
