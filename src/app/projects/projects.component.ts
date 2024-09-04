5f4b9fcfd68130e58805708d6a8c0d84df4b01f6
Chris Muzzin <chrismuzzin@yahoo.com>
2024-09-03 19:54:00 -0700

    update screen shots

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { register } from 'swiper/element/bundle';
+
 register();
 
 @Component({
@@ -42,36 +43,36 @@ export class ProjectsComponent {
     'spc-images/spc10.png',
   ];
 
-  rmchPhotos = [
-    'rmch-photos/rmch1.PNG',
-    'rmch-photos/rmch2.PNG',
-    'rmch-photos/rmch3.PNG',
-    'rmch-photos/rmch4.PNG',
-    'rmch-photos/rmch5.PNG',
-    'rmch-photos/rmch6.PNG',
-    'rmch-photos/rmch7.PNG',
-    'rmch-photos/rmch8.PNG',
-    'rmch-photos/rmch9.PNG',
-    'rmch-photos/rmch10.PNG',
-    'rmch-photos/rmch11.PNG',
-    'rmch-photos/rmch12.PNG',
-    'rmch-photos/rmch13.PNG',
-    'rmch-photos/rmch14.PNG',
+  rmchPhonePhotos = [
+    'rmch_phone/login.png',
+    'rmch_phone/signup.png',
+    'rmch_phone/map.png',
+    'rmch_phone/preview.png',
+    'rmch_phone/tab2.png',
+    'rmch_phone/search.png',
+    'rmch_phone/suggest.png',
+    'rmch_phone/settings.png',
+    'rmch_phone/profile.png',
+    'rmch_phone/shop1.png',
+    'rmch_phone/shop2.png',
+    'rmch_phone/shop3.png',
+    'rmch_phone/noreview.png',
+    'rmch_phone/roast.png',
   ];
 
-  rmchWebPhotos = [
-    'rmch_web/login.PNG',
-    'rmch_web/tab1.PNG',
-    'rmch_web/tab2.PNG',
-    'rmch_web/tab3.PNG',
-    'rmch_web/popover.PNG',
-    'rmch_web/profile.PNG',
-    'rmch_web/search.PNG',
-    'rmch_web/suggest.PNG',
-    'rmch_web/suggestions.PNG',
-    'rmch_web/dataeditor.PNG',
-    'rmch_web/shop.PNG',
-    'rmch_web/roast.PNG',
-
+  rmchTabletPhotos = [
+    'rmch_tablet/login.png',
+    'rmch_tablet/signup.png',
+    'rmch_tablet/map.png',
+    'rmch_tablet/preview.png',
+    'rmch_tablet/tab2.png',
+    'rmch_tablet/search.png',
+    'rmch_tablet/suggest.png',
+    'rmch_tablet/settings.png',
+    'rmch_tablet/profile.png',
+    'rmch_tablet/shop.png',
+    'rmch_tablet/noreview.png',
+    'rmch_tablet/roast.png',
+    'rmch_tablet/roas2.png',
   ]
 }
